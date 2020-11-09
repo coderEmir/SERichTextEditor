8f29cd0a72ba3fbcd4a4399d634d1917a2f6edad
wc <864009759@qq.com>
2020-11-09 14:37:38 +0800

    Initial Commit

---
+++ SERichTextEditor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SERichTextEditor
+//
+//  Created by wenchang on 2020/11/9.
+//  Copyright © 2020 seeEmil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SERichTextEditor/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  SERichTextEditor
+//
+//  Created by wenchang on 2020/11/9.
+//  Copyright © 2020 seeEmil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
